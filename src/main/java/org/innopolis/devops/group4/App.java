21ee93410759cc7882446de9060d697a74f9be94
Amir Nazyrov <a.nazyroff@mail.ru>
2020-04-16 02:20:34 +0300

    Initial commit

---
+++ src/main/java/org/innopolis/devops/group4/App.java
@@ -0,0 +1,16 @@
+package org.innopolis.devops.group4;
+
+public class App {
+
+    private final String message = "Hello World!";
+
+    public App() {}
+
+    public static void main(String[] args) {
+        System.out.println(new App().getMessage());
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
